d76abcca50b1d1957bd6e8d2a1476ac23005b6ac
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-02-11 17:07:38 +0530

    Update Context.jsx

--- FRONTEND/src/Context.jsx
+++ FRONTEND/src/Context.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext();
 export const StoreContextProvider = ({ children }) => {
   const [token, setToken] = useState("");
   //const url = "https://library-management-system-backend-nac4.onrender.com";
-  const API_BASE_URL = import.meta.env.VITE_API_URL;
+  const url = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     const storedToken = localStorage.getItem("user_token");
@@ -19,8 +19,7 @@ export const StoreContextProvider = ({ children }) => {
   const storeVal = {
     token,
     setToken,
-    //url,
-    API_BASE_URL,
+    url,
   };
 
   return (
